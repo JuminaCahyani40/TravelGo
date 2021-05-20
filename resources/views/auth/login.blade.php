b1a0c4fd679a4c2beaaa99bbac139198a71281c4
JuminaCahyani40 <60830909+JuminaCahyani40@users.noreply.github.com>
2021-05-20 09:35:54 +0800

    ld page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,7 +1,10 @@
 <x-guest-layout>
     <x-jet-authentication-card>
         <x-slot name="logo">
-            <x-jet-authentication-card-logo />
+            <a class="navbar-brand" href="#">
+                <img src="/img/logo_tg.png" alt="logo" style="width: 300px;" >
+            </a>
+            <!--<x-jet-authentication-card-logo />-->
         </x-slot>
 
         <x-jet-validation-errors class="mb-4" />
